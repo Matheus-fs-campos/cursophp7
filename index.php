389bad251c4bb05fa1c12f4d8a2c777dd3a783df
Matheus Campos <matheusfscampos@gmail.com>
2021-03-04 22:53:02 -0300

    commit

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-    require ("config.php");
-    $sql = new sql();
 
-    $usuarios = $sql ->select("select * from tb_usuarios");
-    echo json_encode($usuarios);
+    require ("config.php");
+//primeira parte
+    //$sql = new sql();
+    //$usuarios = $sql ->select("select * from tb_usuarios");
+    //echo json_encode($usuarios);
+
+
+//Segunda parte
+
+    $root = new Usuario();
+    $root->loadById(1);
+    echo $root;
 ?>
